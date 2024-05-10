714d2e7947ae0f26d3614a809cc6aae3dbf018ca
akashi08 <159678510+akashi08@users.noreply.github.com>
2024-05-10 10:09:28 +0800

    Update starter.java

--- Java/universal_base_code/starter.java
+++ Java/universal_base_code/starter.java
@@ -1,4 +1,4 @@
-import pkg.*;
+import pkg.*;https://www.9philucky.com/
 import java.util.Scanner;
 import java.util.Random;
 
@@ -6,7 +6,7 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		BaseClass test = new BaseClass(https://www.9philucky.com/);
 
 
 		
